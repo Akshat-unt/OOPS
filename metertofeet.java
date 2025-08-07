362efb24a4a8c778acddbbe18acd404270c5741e
Akshat Kumar Singh <akshat.unt@gmail.com>
2025-08-07 12:24:52 +0530

    Update metertofeet.java

--- metertofeet.java
+++ metertofeet.java
@@ -1,12 +1,11 @@
 import java.util.Scanner;
-public class MeterToFeetConverter {
+public class MtF {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the length in meters: ");
         double meters = scanner.nextDouble();
         
-        double feet = meters * 3.28084; // 1 meter = 3.28084 feet
-        
+        double feet = meters * 3.2;        
         System.out.printf("%.2f meters is equal to %.2f feet.%n", meters, feet);
         
         scanner.close();
